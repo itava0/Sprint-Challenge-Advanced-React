7da886ebbf711b73612ef8c1f53c02330474f244
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-10-04 13:06:36 -0400

    Created a class component to fetch all the data and to passed down as props to childs componenets

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Players from "./components/Players";
+import Navbar from "./components/Navbar";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    player: []
+  };
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players").then(res => {
+      this.setState({
+        player: [...res.data]
+      });
+    });
+  }
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className="container">
+          <Players player={this.state.player} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
